3d1d9513b7cd01a41248018e177a1b741f95cad2
xSomoy <xsomoy@tutanota.com>
2020-08-25 19:33:59 +0700

    structuring

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Main Counter Start
 
-setInterval(minCounter, 500);
+
 
 function minCounter() {
     var now = new moment();
@@ -14,5 +14,25 @@ function minCounter() {
     document.getElementById("left").innerHTML = minLeft + ':' + inSec;
 }
 
+setInterval(minCounter, 500);
+
 // Main Counter End
+
+// Time Sections
+
+var work = 666;
+var sleep = 420;
+var general = 120;
+var reading = 60;
+var socialMedia = 54;
+var personal = 60;
+var buffer = 60;
+
+document.getElementById("work").innerHTML = 'Work Min Left: ' + work;
+document.getElementById("sleep").innerHTML = 'Sleep Min Left: ' + sleep;
+document.getElementById("general").innerHTML = 'GeneralMin Left: ' + general;
+document.getElementById("reading").innerHTML = 'Reading Min Left: ' + reading;
+document.getElementById("socialMedia").innerHTML = 'Social MediaMin Left: ' + socialMedia;
+document.getElementById("personal").innerHTML = 'Perosonal Min Left: ' + personal;
+document.getElementById("buffer").innerHTML = 'Buffer Min Left: ' + buffer;
 
