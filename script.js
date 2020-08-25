5d67a00b9a48bb1e697122900cb7a2364b2dd627
xSomoy <xsomoy@tutanota.com>
2020-08-25 19:09:51 +0700

    working

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(minCounter, 500);
+
+function minCounter() {
+    var now = new moment();
+    var s = now.format("ss");
+    var h = now.format("HH");
+    var m = now.format("mm");
+    var minGone = ((h*60) + Number(m));
+    var minLeft = 1440 - Number(minGone);
+    var inSec = 60 - Number(s);
+    document.getElementById("time").innerHTML = minGone + ':' + s;
+    document.getElementById("left").innerHTML = minLeft + ':' + inSec;
+}
