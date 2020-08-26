71c03206704eaf66793c5af9aa752b0a2c815cc8
xSomoy <xsomoy@tutanota.com>
2020-08-26 15:46:35 +0700

    Wroking Toggle Control

--- js/script.js
+++ js/script.js
@@ -20,104 +20,247 @@ setInterval(minCounter, 500);
 
 // Function Section
 
-var work = 666;
-var sleep = 420;
-var general = 120;
-var reading = 60;
-var socialMedia = 54;
-var personal = 60;
-var buffer = 60;
-var working = 0;
+const workButton = document.getElementById('workButton');
+const sleepButton = document.getElementById('sleepButton');
+const generalButton = document.getElementById('generalButton');
+const readingButton = document.getElementById('readingButton');
+const socialButton = document.getElementById('socialButton');
+const personalButton = document.getElementById('personalButton');
+const bufferButton = document.getElementById('bufferButton');
 
 // Toggle Functions
 
 function Work() {
-    var status = document.getElementById('workButton').innerText;
-    if ( status == "Work") {
-        document.getElementById('workButton').innerHTML = 'Work Time';
-        document.getElementById('workButton').style.background = 'green';
+    var status = workButton.innerText;
+    if (status == "Work") {
+        
+        workButton.innerHTML = 'Work Time';
+        workButton.style.background = 'green';
+
+        // Toggle Other Button
+        
+        sleepButton.innerHTML = 'Sleep';
+        sleepButton.style.background = 'red';
+
+        generalButton.innerHTML = 'General';
+        generalButton.style.background = 'red';
+
+        readingButtton.innerHTML = 'Read';
+        readingButtton.style.background = 'red';
+
+        socialButton.innerHTML = 'Social';
+        socialButton.style.background = 'red';
+
+        personalButton.innerHTML = 'Personal';
+        personalButton.style.background = 'red';
+
+        bufferButton.innerHTML = 'Buffer';
+        bufferButton.style.background = 'red';
+
 
     } else if (status == "Work Time") {
-        document.getElementById('workButton').innerHTML = 'Work';
-        document.getElementById('workButton').style.background = 'red';
+        workButton.innerHTML = 'Work';
+        workButton.style.background = 'red';
 
     }
 }
 
 function Sleep() {
-    var status = document.getElementById('sleepButton').innerText;
-    if ( status == "Sleep") {
-        document.getElementById('sleepButton').innerHTML = 'Sleep Time';
-        document.getElementById('sleepButton').style.background = 'green';
+    var status = sleepButton.innerText;
+    if (status == "Sleep") {
+        
+        sleepButton.innerHTML = 'Sleep Time';
+        sleepButton.style.background = 'green';
+        
+         // Toggle Other Button
+        
+        workButton.innerHTML = 'Work';
+        workButton.style.background = 'red';
+
+        generalButton.innerHTML = 'General';
+        generalButton.style.background = 'red';
+
+        readingButtton.innerHTML = 'Read';
+        readingButtton.style.background = 'red';
+
+        socialButton.innerHTML = 'Social';
+        socialButton.style.background = 'red';
+
+        personalButton.innerHTML = 'Personal';
+        personalButton.style.background = 'red';
+
+        bufferButton.innerHTML = 'Buffer';
+        bufferButton.style.background = 'red';
 
     } else if (status == "Sleep Time") {
-        document.getElementById('sleepButton').innerHTML = 'Sleep';
-        document.getElementById('sleepButton').style.background = 'red';
+        sleepButton.innerHTML = 'Sleep';
+        sleepButton.style.background = 'red';
 
     }
 }
 
 function General() {
-    var status = document.getElementById('generalButton').innerText;
-    if ( status == "General") {
-        document.getElementById('generalButton').innerHTML = 'General Time';
-        document.getElementById('generalButton').style.background = 'green';
+    var status = generalButton.innerText;
+    if (status == "General") {
+        
+        generalButton.innerHTML = 'General Time';
+        generalButton.style.background = 'green';
+        
+         // Toggle Other Button
+        
+        sleepButton.innerHTML = 'Sleep';
+        sleepButton.style.background = 'red';
+
+        workButton.innerHTML = 'Work';
+        workButton.style.background = 'red';
+
+        readingButtton.innerHTML = 'Read';
+        readingButtton.style.background = 'red';
+
+        socialButton.innerHTML = 'Social';
+        socialButton.style.background = 'red';
+
+        personalButton.innerHTML = 'Personal';
+        personalButton.style.background = 'red';
+
+        bufferButton.innerHTML = 'Buffer';
+        bufferButton.style.background = 'red';
 
     } else if (status == "General Time") {
-        document.getElementById('generalButton').innerHTML = 'General';
-        document.getElementById('generalButton').style.background = 'red';
+        generalButton.innerHTML = 'General';
+        generalButton.style.background = 'red';
 
     }
 }
 
 function Reading() {
-    var status = document.getElementById('readingButton').innerText;
-    if ( status == "Reading") {
-        document.getElementById('readingButton').innerHTML = 'Reading Time';
-        document.getElementById('readingButton').style.background = 'green';
+    var status = readingButtton.innerText;
+    if (status == "Reading") {
+        readingButtton.innerHTML = 'Reading Time';
+        readingButtton.style.background = 'green';
+        
+         // Toggle Other Button
+        
+        sleepButton.innerHTML = 'Sleep';
+        sleepButton.style.background = 'red';
+
+        generalButton.innerHTML = 'General';
+        generalButton.style.background = 'red';
+
+        workButtton.innerHTML = 'Work';
+        workButtton.style.background = 'red';
+
+        socialButton.innerHTML = 'Social';
+        socialButton.style.background = 'red';
+
+        personalButton.innerHTML = 'Personal';
+        personalButton.style.background = 'red';
+
+        bufferButton.innerHTML = 'Buffer';
+        bufferButton.style.background = 'red';
 
     } else if (status == "Reading Time") {
-        document.getElementById('readingButton').innerHTML = 'Read';
-        document.getElementById('readingButton').style.background = 'red';
+        readingButtton.innerHTML = 'Read';
+        readingButtton.style.background = 'red';
 
     }
 }
 
-function SocialMedia() {
-    var status = document.getElementById('socialMediaButton').innerText;
-    if ( status == "Social Media") {
-        document.getElementById('socialMediaButton').innerHTML = 'Social Media Time';
-        document.getElementById('socialMediaButton').style.background = 'green';
+function Social() {
+    var status = socialButton.innerText;
+    if (status == "Social") {
+        socialButton.innerHTML = 'Social Time';
+        socialButton.style.background = 'green';
+        
+         // Toggle Other Button
+        
+        sleepButton.innerHTML = 'Sleep';
+        sleepButton.style.background = 'red';
 
-    } else if (status == "Social Media Time") {
-        document.getElementById('socialMediaButton').innerHTML = 'Social Media';
-        document.getElementById('socialMediaButton').style.background = 'red';
+        generalButton.innerHTML = 'General';
+        generalButton.style.background = 'red';
+
+        readingButtton.innerHTML = 'Read';
+        readingButtton.style.background = 'red';
+
+        workButton.innerHTML = 'Work';
+        workButton.style.background = 'red';
+
+        personalButton.innerHTML = 'Personal';
+        personalButton.style.background = 'red';
+
+        bufferButton.innerHTML = 'Buffer';
+        bufferButton.style.background = 'red';
+
+    } else if (status == "Social Time") {
+        socialButton.innerHTML = 'Social';
+        socialButton.style.background = 'red';
 
     }
 }
 
 function Personal() {
-    var status = document.getElementById('personalButton').innerText;
-    if ( status == "Personal") {
-        document.getElementById('personalButton').innerHTML = 'Personal Time';
-        document.getElementById('personalButton').style.background = 'green';
+    var status = personalButton.innerText;
+    if (status == "Personal") {
+        personalButton.innerHTML = 'Personal Time';
+        personalButton.style.background = 'green';
+        
+         // Toggle Other Button
+        
+        sleepButton.innerHTML = 'Sleep';
+        sleepButton.style.background = 'red';
+
+        generalButton.innerHTML = 'General';
+        generalButton.style.background = 'red';
+
+        readingButtton.innerHTML = 'Read';
+        readingButtton.style.background = 'red';
+
+        socialButton.innerHTML = 'Social';
+        socialButton.style.background = 'red';
+
+        workButton.innerHTML = 'Work';
+        workButton.style.background = 'red';
+
+        bufferButton.innerHTML = 'Buffer';
+        bufferButton.style.background = 'red';
 
     } else if (status == "Personal Time") {
-        document.getElementById('personalButton').innerHTML = 'Personal';
-        document.getElementById('personalButton').style.background = 'red';
+        personalButton.innerHTML = 'Personal';
+        personalButton.style.background = 'red';
 
     }
 }
 
 function Buffer() {
-    var status = document.getElementById('bufferButton').innerText;
-    if ( status == "Buffer") {
-        document.getElementById('bufferButton').innerHTML = 'Buffer Time';
-        document.getElementById('bufferButton').style.background = 'green';
+    var status = bufferButton.innerText;
+    if (status == "Buffer") {
+        bufferButton.innerHTML = 'Buffer Time';
+        bufferButton.style.background = 'green';
+        
+         // Toggle Other Button
+        
+        sleepButton.innerHTML = 'Sleep';
+        sleepButton.style.background = 'red';
+
+        generalButton.innerHTML = 'General';
+        generalButton.style.background = 'red';
+
+        readingButtton.innerHTML = 'Read';
+        readingButtton.style.background = 'red';
+
+        socialButton.innerHTML = 'Social';
+        socialButton.style.background = 'red';
+
+        personalButton.innerHTML = 'Personal';
+        personalButton.style.background = 'red';
+
+        workButton.innerHTML = 'Work';
+        workButton.style.background = 'red';
 
     } else if (status == "Buffer Time") {
-        document.getElementById('bufferButton').innerHTML = 'Buffer';
-        document.getElementById('bufferButton').style.background = 'red';
+        bufferButton.innerHTML = 'Buffer';
+        bufferButton.style.background = 'red';
 
     }
 }
@@ -125,11 +268,19 @@ function Buffer() {
 
 
 // Time Sections
-
-document.getElementById("work").innerHTML = 'Work Min Left: ' + work;
-document.getElementById("sleep").innerHTML = 'Sleep Min Left: ' + sleep;
-document.getElementById("general").innerHTML = 'General Min Left: ' + general;
-document.getElementById("reading").innerHTML = 'Reading Min Left: ' + reading;
-document.getElementById("socialMedia").innerHTML = 'Social MediaMin Left: ' + socialMedia;
-document.getElementById("personal").innerHTML = 'Perosonal Min Left: ' + personal;
-document.getElementById("buffer").innerHTML = 'Buffer Min Left: ' + buffer;
+//var work = 666;
+//var sleep = 420;
+//var general = 120;
+//var reading = 60;
+//var socialMedia = 54;
+//var personal = 60;
+//var buffer = 60;
+//var working = 0;
+//
+//document.getElementById("work").innerHTML = 'Work Min Left: ' + work;
+//document.getElementById("sleep").innerHTML = 'Sleep Min Left: ' + sleep;
+//document.getElementById("general").innerHTML = 'General Min Left: ' + general;
+//document.getElementById("reading").innerHTML = 'Reading Min Left: ' + reading;
+//document.getElementById("socialMedia").innerHTML = 'SocialMin Left: ' + socialMedia;
+//document.getElementById("personal").innerHTML = 'Perosonal Min Left: ' + personal;
+//document.getElementById("buffer").innerHTML = 'Buffer Min Left: ' + buffer;
