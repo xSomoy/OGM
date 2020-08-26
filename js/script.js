dea46277fca9e9b16dc22a2c24f041a52552d06f
xSomoy <xsomoy@tutanota.com>
2020-08-26 23:42:44 +0700

    WOrking On Section Activition

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 // Main Counter Start
 
+const workButton = document.getElementById('workButton');
+const sleepButton = document.getElementById('sleepButton');
+const generalButton = document.getElementById('generalButton');
+const readingButton = document.getElementById('readingButton');
+const socialButton = document.getElementById('socialButton');
+const personalButton = document.getElementById('personalButton');
+const bufferButton = document.getElementById('bufferButton');
 
+var work = 666;
+var sleep = 420;
+var general = 120;
+var reading = 60;
+var socialMedia = 54;
+var personal = 60;
+var buffer = 60;
+var working = 0;
 
 function minCounter() {
     var now = new moment();
@@ -18,15 +33,24 @@ setInterval(minCounter, 500);
 
 // Main Counter End
 
+//Time Sections
+
+
+
+
+
+//document.getElementById("work").innerHTML = 'Work Min Left: ' + work;
+document.getElementById("sleep").innerHTML = 'Sleep Min Left: ' + sleep;
+document.getElementById("general").innerHTML = 'General Min Left: ' + general;
+document.getElementById("reading").innerHTML = 'Reading Min Left: ' + reading;
+document.getElementById("socialMedia").innerHTML = 'SocialMin Left: ' + socialMedia;
+document.getElementById("personal").innerHTML = 'Perosonal Min Left: ' + personal;
+document.getElementById("buffer").innerHTML = 'Buffer Min Left: ' + buffer;
+
+
 // Function Section
 
-const workButton = document.getElementById('workButton');
-const sleepButton = document.getElementById('sleepButton');
-const generalButton = document.getElementById('generalButton');
-const readingButton = document.getElementById('readingButton');
-const socialButton = document.getElementById('socialButton');
-const personalButton = document.getElementById('personalButton');
-const bufferButton = document.getElementById('bufferButton');
+
 
 // Toggle Functions
 
@@ -161,8 +185,8 @@ function Reading() {
         bufferButton.style.background = 'red';
 
     } else if (status == "Reading Time") {
-        generalButton.innerHTML = 'Reading';
-        generalButton.style.background = 'red';
+        readingButton.innerHTML = 'Reading';
+        readingButton.style.background = 'red';
 
     }
 }
@@ -270,23 +294,3 @@ function Buffer() {
 
     }
 }
-
-
-
-// Time Sections
-//var work = 666;
-//var sleep = 420;
-//var general = 120;
-//var reading = 60;
-//var socialMedia = 54;
-//var personal = 60;
-//var buffer = 60;
-//var working = 0;
-//
-//document.getElementById("work").innerHTML = 'Work Min Left: ' + work;
-//document.getElementById("sleep").innerHTML = 'Sleep Min Left: ' + sleep;
-//document.getElementById("general").innerHTML = 'General Min Left: ' + general;
-//document.getElementById("reading").innerHTML = 'Reading Min Left: ' + reading;
-//document.getElementById("socialMedia").innerHTML = 'SocialMin Left: ' + socialMedia;
-//document.getElementById("personal").innerHTML = 'Perosonal Min Left: ' + personal;
-//document.getElementById("buffer").innerHTML = 'Buffer Min Left: ' + buffer;
