e2697478ed92f26378c516087c04000c7a60fca8
xSomoy <xsomoy@tutanota.com>
2020-08-28 14:44:24 +0700

    Section Activition Done

--- js/script.js
+++ js/script.js
@@ -191,8 +191,8 @@ function Social() {
         bufferButton.style.background = 'red';
 
     } else if (status == "Social Time") {
-        generalButton.innerHTML = 'Social';
-        generalButton.style.background = 'red';
+        socialButton.innerHTML = 'Social';
+        socialButton.style.background = 'red';
 
     }
 }
@@ -267,20 +267,26 @@ function Buffer() {
     }
 }
 
-//Wrok Section
+
+// Default Time Set
 
 document.getElementById("workTime").innerHTML = 666;
-var workTime = document.getElementById("workTime").innerHTML;
+document.getElementById("sleepTime").innerHTML = 420;
+document.getElementById("generalTime").innerHTML = 120;
+document.getElementById("readingTime").innerHTML = 60;
+document.getElementById("socialTime").innerHTML = 54;
+document.getElementById("personalTime").innerHTML = 60;
+document.getElementById("bufferTime").innerHTML = 60;
 
-function Timer() {
-    workTime = workTime - 1;
-    document.getElementById("workTime").innerHTML = workTime;
-}
+
+// Wrok Counter
 
 function workCounter() {
     var status = workButton.innerText;
     if (status == 'Work Time') {
-        Timer();
+        var workTime = document.getElementById("workTime").innerHTML;
+        workTime = workTime - 1;
+        document.getElementById("workTime").innerHTML = workTime;
 
     } else {
         var work = document.getElementById("workTime").innerHTML;
@@ -290,24 +296,103 @@ function workCounter() {
 setInterval(workCounter, 6000);
 
 
-//Sleep Section
+// Sleep Counter
 
-document.getElementById("sleepTime").innerHTML = 420;
-var workTime = document.getElementById("sleepTime").innerHTML;
-
-function Timer() {
-    workTime = workTime - 1;
-    document.getElementById("sleepTime").innerHTML = workTime;
-}
-
-function workCounter() {
+function sleepCounter() {
     var status = sleepButton.innerText;
     if (status == 'Sleep Time') {
-        Timer();
+        var sleepTime = document.getElementById("sleepTime").innerHTML;
+        sleepTime = sleepTime - 1;
+        document.getElementById("sleepTime").innerHTML = sleepTime;
 
     } else {
-        var work = document.getElementById("sleepTime").innerHTML;
-        document.getElementById("sleepTime").innerHTML = work;
+        var sleep = document.getElementById("sleepTime").innerHTML;
+        document.getElementById("sleepTime").innerHTML = sleep;
     }
 }
-setInterval(workCounter, 6000);
+setInterval(sleepCounter, 6000);
+
+
+// General Counter
+
+function generalCounter() {
+    var status = generalButton.innerText;
+    if (status == 'General Time') {
+        var generalTime = document.getElementById("generalTime").innerHTML;
+        generalTime = generalTime - 1;
+        document.getElementById("generalTime").innerHTML = generalTime;
+
+    } else {
+        var general = document.getElementById("generalTime").innerHTML;
+        document.getElementById("generalTime").innerHTML = general;
+    }
+}
+setInterval(generalCounter, 6000);
+
+
+// Reading Counter
+
+function readingCounter() {
+    var status = readingButton.innerText;
+    if (status == 'Reading Time') {
+        var readingTime = document.getElementById("readingTime").innerHTML;
+        readingTime = readingTime - 1;
+        document.getElementById("readingTime").innerHTML = readingTime;
+
+    } else {
+        var reading = document.getElementById("readingTime").innerHTML;
+        document.getElementById("readingTime").innerHTML = reading;
+    }
+}
+setInterval(readingCounter, 6000);
+
+
+// Social Counter
+
+function socialCounter() {
+    var status = socialButton.innerText;
+    if (status == 'Social Time') {
+        var socialTime = document.getElementById("socialTime").innerHTML;
+        socialTime = socialTime - 1;
+        document.getElementById("socialTime").innerHTML = socialTime;
+
+    } else {
+        var social = document.getElementById("socialTime").innerHTML;
+        document.getElementById("socialTime").innerHTML = social;
+    }
+}
+setInterval(socialCounter, 6000);
+
+
+// Personal Counter
+
+function personalCounter() {
+    var status = personalButton.innerText;
+    if (status == 'Personal Time') {
+        var personalTime = document.getElementById("personalTime").innerHTML;
+        personalTime = personalTime - 1;
+        document.getElementById("personalTime").innerHTML = personalTime;
+
+    } else {
+        var personal = document.getElementById("personalTime").innerHTML;
+        document.getElementById("personalTime").innerHTML = personal;
+    }
+}
+setInterval(personalCounter, 6000);
+
+
+// Buffer Counter
+
+function bufferCounter() {
+    var status = bufferButton.innerText;
+    if (status == 'Buffer Time') {
+        var bufferTime = document.getElementById("bufferTime").innerHTML;
+        bufferTime = bufferTime - 1;
+        document.getElementById("bufferTime").innerHTML = bufferTime;
+
+    } else {
+        var buffer = document.getElementById("bufferTime").innerHTML;
+        document.getElementById("bufferTime").innerHTML = buffer;
+    }
+}
+setInterval(bufferCounter, 6000);
